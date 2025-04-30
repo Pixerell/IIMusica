9ac0d51ee9253a3a57ce011ad2e8c08c2b06e5eb
Pixerell <alexmegaflexer@gmail.com>
2025-04-30 19:17:28 +0300

    Way better scrollbar. draggable, optimized and with fade animations

--- app/src/main/java/com/example/iimusica/components/ux/LazyColumnScrollBar.kt
+++ app/src/main/java/com/example/iimusica/components/ux/LazyColumnScrollBar.kt
@@ -6,18 +6,22 @@ import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -25,9 +29,10 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
-import androidx.media3.common.util.Log
 import androidx.media3.common.util.UnstableApi
 import com.example.iimusica.ui.theme.LocalAppColors
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @OptIn(UnstableApi::class)
@@ -35,109 +40,79 @@ import kotlinx.coroutines.launch
 fun LazyColumnScrollBar(
     lazyListState: LazyListState,
     width: Int = 10,
+    avgItemHeight: Int = 85,
+    bottomPadding: Int = 124
 ) {
     val appColors = LocalAppColors.current
     val density = LocalDensity.current
 
-    val closeItemHeight = with(density) { 83.dp.toPx() }
-    val totalItemCount = lazyListState.layoutInfo.totalItemsCount.takeIf { it > 0 } ?: 1
-    val viewportHeight = lazyListState.layoutInfo.viewportSize.height
-    val bottomPaddingPx = with(density) { 120.dp.toPx() }
-
-    val thumbHeightPx = remember(totalItemCount, viewportHeight) {
-        val contentHeight = totalItemCount * closeItemHeight + bottomPaddingPx
-        val ratio = viewportHeight / contentHeight
-        (ratio * viewportHeight).coerceIn(0f, viewportHeight.toFloat() * 0.4f)
-    }
-
-
-    val topOffset by remember(lazyListState) {
-        derivedStateOf {
-            val totalCnt = lazyListState.layoutInfo.totalItemsCount.takeIf { it > 0 } ?: 1
-            val visibleCnt =
-                lazyListState.layoutInfo.visibleItemsInfo.count().takeIf { it > 0 } ?: 1
-            val columnHeight = lazyListState.layoutInfo.viewportSize.height
-            val firstVisibleIndex = lazyListState.firstVisibleItemIndex
-            val scrollItemHeight = (columnHeight.toFloat() / totalCnt)
-            val realItemHeight = (columnHeight.toFloat() / visibleCnt)
-            val offset = ((firstVisibleIndex) * scrollItemHeight)
-            val firstItemOffset =
-                lazyListState.firstVisibleItemScrollOffset / realItemHeight * scrollItemHeight
-
-            offset + firstItemOffset
-        }
-    }
-
     val scope = rememberCoroutineScope()
+    val horizontalOffset = 40f
+    val gesturePadding = 10.dp
 
-    val columnSize by remember(lazyListState) {
+    val avgItemHeightPx = with(density) {avgItemHeight.dp.toPx() }
+    val bottomPaddingPx = with(density) {bottomPadding.dp.toPx() }
+
+    val totalItemCount by remember(lazyListState) {
+        derivedStateOf { lazyListState.layoutInfo.totalItemsCount.takeIf { it > 0 } ?: 1 }
+    }
+    val scrollHeight by remember(lazyListState) {
+        derivedStateOf { lazyListState.layoutInfo.viewportSize.height }
+    }
+    val contentHeightPx = remember(totalItemCount, scrollHeight) {
+        totalItemCount * avgItemHeightPx + bottomPaddingPx
+    }
+    val thumbHeightPx = remember(contentHeightPx, scrollHeight) {
+        ((scrollHeight / contentHeightPx) * scrollHeight)
+            .coerceIn(0f, scrollHeight * 0.4f)
+    }
+    val maxScrollPx by remember(contentHeightPx, scrollHeight) {
         derivedStateOf {
-            lazyListState.layoutInfo.viewportSize
+            (contentHeightPx - scrollHeight).coerceAtLeast(1f)
         }
     }
 
-    val thumbHeight = thumbHeightPx.coerceIn(0f, columnSize.height.toFloat() * 0.4f)
-    val scrollHeight = lazyListState.layoutInfo.viewportSize.height
-
-
-    val estimatedItemHeight by remember {
-        mutableStateOf(51f) // or whatever a typical item height is
-    }
-
-    val estimatedThumbHeight = remember(lazyListState) {
+    val scrolledPx by remember(lazyListState) {
         derivedStateOf {
-            val totalCount = lazyListState.layoutInfo.totalItemsCount.takeIf { it > 0 } ?: 1
-            val viewportHeight = lazyListState.layoutInfo.viewportSize.height
-
-            val totalContentHeight = totalCount * closeItemHeight + bottomPaddingPx
-            val thumbRatio = viewportHeight / totalContentHeight
-
-            (thumbRatio * viewportHeight).coerceAtMost(viewportHeight.toFloat())
-        }
-    }
-
-    val hasStableLayout = remember(lazyListState) {
-        derivedStateOf {
-            val visibleInfo = lazyListState.layoutInfo.visibleItemsInfo
-            val totalItems = lazyListState.layoutInfo.totalItemsCount
-            val enoughItems = visibleInfo.size >= 3
-            val coversEnough = (visibleInfo.firstOrNull()?.index ?: 0) > 0 &&
-                    (visibleInfo.lastOrNull()?.index ?: 0) < totalItems - 1
-
-            enoughItems && coversEnough
+            val firstIndex = lazyListState.firstVisibleItemIndex
+            val firstOffset = lazyListState.firstVisibleItemScrollOffset.toFloat()
+            firstIndex * avgItemHeightPx + firstOffset
         }
     }
 
 
-    Log.d("scrollershit3", "finalTH - ${hasStableLayout.value}")
-    val finalThumbHeight = if (hasStableLayout.value) {
-        thumbHeight
-    } else {
-        estimatedThumbHeight.value.coerceAtMost(scrollHeight.toFloat())
-    }
-
-    val finalTopOffset= if (hasStableLayout.value){
-        topOffset
-    } else {
-        val index = lazyListState.firstVisibleItemIndex
-        index * estimatedItemHeight
-    }
+    val rawThumbOffset = (scrolledPx / maxScrollPx) * (scrollHeight - thumbHeightPx)
+    val topOffsetPx = rawThumbOffset.coerceIn(0f, scrollHeight - thumbHeightPx)
 
     val animatedThumbHeight by animateFloatAsState(
-        targetValue = finalThumbHeight,
-        animationSpec = spring(stiffness = Spring.StiffnessLow),
-        label = "AnimatedThumbHeight"
+        targetValue = thumbHeightPx,
+        animationSpec = spring(stiffness = Spring.StiffnessLow)
     )
-
-
-
     val animatedTopOffset by animateFloatAsState(
-        targetValue = finalTopOffset,
-        animationSpec = spring(stiffness = Spring.StiffnessLow),
-        label = "AnimatedTopOffset"
+        targetValue = topOffsetPx,
+        animationSpec = spring(stiffness = Spring.StiffnessLow)
     )
 
-    Log.d("scrollershit2", "finalTH - $finalThumbHeight TH - $thumbHeight and ETH ${estimatedThumbHeight.value.coerceAtMost(scrollHeight.toFloat())}")
+    // Animation for fadeins/outs of when you are scrolling
+    var isScrolling by remember { mutableStateOf<Boolean>(false) }
+    var fadeOutJob: Job? by remember { mutableStateOf(null) }
+    val thumbAlpha by animateFloatAsState(
+        targetValue = if (isScrolling) 1f else 0f,
+        animationSpec = spring(stiffness = Spring.StiffnessLow)
+    )
+    // Look for changes in scrolling, if you are - no fadeouts. after pause - fadeout
+    LaunchedEffect(scrolledPx) {
+        if (scrolledPx > 0) {
+            if (!isScrolling) {
+                isScrolling = true
+            }
+        }
+        fadeOutJob?.cancel()
+        fadeOutJob = scope.launch {
+            delay(1000)
+            isScrolling = false
+        }
+    }
 
     Box(
         modifier = Modifier.fillMaxSize(),
@@ -145,8 +120,23 @@ fun LazyColumnScrollBar(
     ) {
         Canvas(
             modifier = Modifier
-                .width(width.dp)
+                .width(gesturePadding+width.dp)
                 .fillMaxHeight()
+                .pointerInput(Unit) {
+                    detectDragGestures { change, dragAmount ->
+                        change.consume()
+
+
+                        val scrollDelta = dragAmount.y
+                        val scrollFraction = scrollDelta / scrollHeight
+                        val contentScrollDelta = scrollFraction * contentHeightPx
+                        isScrolling = true
+
+                        scope.launch {
+                            lazyListState.scrollBy(contentScrollDelta)
+                        }
+                    }
+                }
                 .pointerInput(Unit) {
                     detectTapGestures { tapOffset ->
                         val scrollFraction = tapOffset.y / scrollHeight
@@ -159,8 +149,8 @@ fun LazyColumnScrollBar(
                 },
             onDraw = {
                 drawRect(
-                    color = appColors.activeDarker,
-                    topLeft = Offset(16f, animatedTopOffset),
+                    color = appColors.activeDarker.copy(alpha = thumbAlpha),
+                    topLeft = Offset(horizontalOffset, animatedTopOffset),
                     size = Size(width.toFloat(), animatedThumbHeight)
                 )
             }
